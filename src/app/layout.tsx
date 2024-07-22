9396436210604c70b708e350ecd8cfee15e30bb8
Justin Byrd <justinbyrd7@gmail.com>
2024-07-22 12:21:05 -0500

    chore: Update background color in layout.tsx and globals.css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="">
       <body className={redditMono.className}>{children}</body>
     </html>
   );
