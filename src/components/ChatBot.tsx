89a1f94d77b25ca3fbdf3659b9fde1771e417b43
Justin Byrd <justinbyrd7@gmail.com>
2024-08-21 12:58:30 -0500

    Refactor page structure and improve layout for better responsiveness

--- src/components/ChatBot.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function ChatBot() {
-  return (
-    <div className="border border-dashed border-black rounded-lg p-2 m-2">
-      AI Chat
-    </div>
-  );
-}
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,20 +1,33 @@
 "use client";
 
 import Image from "next/image";
-import ButtonGroup from "@/components/ButtonGroup";
+import Link from "next/link";
+// import ButtonGroup from "@/components/ButtonGroup";
+import { Button } from "@/components/ui/button";
 import Socials from "@/components/Socials";
 import Nav from "@/components/Nav";
+import headshot from "../../public/assets/headshot.png";
 
 export default function Hero() {
   return (
     <header className="testBorder p-2 m-2 md:w-2/5">
-      {/* <Image src="/hero.jpg" alt="hero" width={1920} height={1080} /> */}
-      <div className="flex size-[250px] testBorder rounded-xl items-center justify-center">
-        <h1 className="text-7xl text-center">image</h1>
-      </div>
-      <h1>Full Name</h1>
-      <h3>Job Title</h3>
-      <ButtonGroup />
+      <Image
+        src={headshot}
+        alt="hero"
+        height={250}
+        width={250}
+        className="rounded-xl"
+      />
+      <h1 className="font-bold">Justin Byrd</h1>
+      <h3 className="subtitle">Full-Stack Web Developer</h3>
+      <Button asChild>
+        <Link
+          href="/assets/Justin Byrd Web Developer Resume.pdf"
+          target="_blank"
+        >
+          Resume
+        </Link>
+      </Button>
       <Socials />
       <Nav /> {/*hidden on mobile */}
     </header>
