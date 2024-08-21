7c6d0b38fd1289f15152538bacac2276eae51d14
Justin Byrd <justinbyrd7@gmail.com>
2024-08-21 13:04:29 -0500

    Refactor page structure and improve layout for better responsiveness

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import About from "./About";
-import ChatBot from "./ChatBot";
 import Education from "./Education";
 import Projects from "./Projects";
 import WorkExperience from "./WorkExperience";
@@ -9,7 +8,6 @@ import WorkExperience from "./WorkExperience";
 export default function Main() {
   return (
     <main className="testBorder flex flex-col gap-2 m-2 md:w-3/5">
-      <ChatBot />
       <About />
       <Projects />
       <WorkExperience />
