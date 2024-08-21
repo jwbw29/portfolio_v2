b36ac5661b67979ede95707b43534161c5d80be0
Justin Byrd <justinbyrd7@gmail.com>
2024-08-21 13:00:40 -0500

    Refactor page structure and improve layout for better responsiveness

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,14 +1,35 @@
 "use client";
 
+import {
+  PiGithubLogoThin,
+  PiLinkedinLogoThin,
+  PiFacebookLogoThin,
+  PiInstagramLogoThin,
+  PiXLogoThin,
+  PiYoutubeLogoThin,
+  PiSnapchatLogoThin,
+} from "react-icons/pi";
+
+const socialUrls = [
+  "http://www.linkedin.com/in/justin-byrd7",
+  "http://www.github.com/jwbw29",
+  "http://www.facebook.com/justin.byrd",
+  "http://www.instagram.com/justin.byrd10",
+  "http://www.x.com/tweeter__byrd",
+  "http://www.youtube.com/jwbw29",
+  "https://t.snapchat.com/plTVcv4U",
+];
+
 export default function Socials() {
   return (
     <div className="flex gap-2">
-      <div className="border border-black size-[25px] rounded-lg"></div>
-      <div className="border border-black size-[25px] rounded-lg"></div>
-      <div className="border border-black size-[25px] rounded-lg"></div>
-      <div className="border border-black size-[25px] rounded-lg"></div>
-      <div className="border border-black size-[25px] rounded-lg"></div>
-      <div className="border border-black size-[25px] rounded-lg"></div>
+      <PiLinkedinLogoThin />
+      <PiGithubLogoThin />
+      <PiFacebookLogoThin />
+      <PiInstagramLogoThin />
+      <PiXLogoThin />
+      <PiYoutubeLogoThin />
+      <PiSnapchatLogoThin />
     </div>
   );
 }
